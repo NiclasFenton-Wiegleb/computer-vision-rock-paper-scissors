be75cab93d93c4de4a4f6c8dce7833acb324cb35
Niclas Fenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-10 18:17:02 +0000

    Camera loop to check input received in python.

--- camera_rps.py
+++ camera_rps.py
@@ -1,5 +1,6 @@
-#from keras.models import load_model
+from keras.models import load_model
 import cv2
+import numpy as np
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
@@ -16,36 +17,19 @@ while(True):
     # the 'q' button is set as the
     # quitting button you may use any
     # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+       break
   
 # After the loop release the cap object
 vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
 
-#The code below creates a preview window and video capture loop
+#Initiate trained model
 
-#vc = cv2.VideoCapture(0)
+model = load_model('keras_model.h5')
 
-#while True:
-    # Capture the video frame by frame
-#    ret, frame = vc.read()
-    #Display the resulting frame
-#    cv2.imshow('frame', frame)
-    #the 'q' button is set as the quitting button
-#    if cv2.waitKey(1) and OxFF == ord('q'):
-#        break
-
-#After the loop release the captured object
-#vid.release()
-#Destroy all windows
-#cv2.destroyAllWindows()
-
-
-#model = load_model('keras_model.h5')
-
-
-#def get_prediction(vc):
-    #predictions = model.predict_classes(vc, verbose =1)
-    #return predictions
+#TO DO: convert 
+def get_prediction(vid):
+    predictions = model.predict_classes(, verbose =1)
+    return predictions
