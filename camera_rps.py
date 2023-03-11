1da89b51307c67434fb3dab33f67322cd044f8cb
Niclas Fenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-11 17:39:19 +0000

    Making get_prediction_from_vid and get_prediction work together

--- camera_rps.py
+++ camera_rps.py
@@ -7,7 +7,24 @@ import time
 #Initiate trained model
 model = load_model('keras_model.h5', compile=False)
 
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+def timer(count_to):
+    #Initiatlises the start time
+    start_time = int(time.time())
+
+    while True:
+        #Current time variable is refreshed with each loop
+        current_time = int(time.time())
+        seconds = current_time - start_time
+        if seconds <= count_to:
+            print(seconds)
+        else:
+            break
+
 def get_prediction(video_input):
+
     # Getting prediction array from model
     predictions_arr = model.predict(video_input, verbose =1)
     #Flattening array and converting to list
@@ -19,17 +36,33 @@ def get_prediction(video_input):
     prediction_index = predictions_lst.index(predictions_flat.max())
     plays = ['Rock', 'Paper', 'Scissors', 'Nothing']
     predicted_play = plays[prediction_index]
-    print(predicted_play)
     return predicted_play
 
-def get_prediction_from_vid():
-    # define a video capture object
-    vid = cv2.VideoCapture(0)
 
-    while(True):
+def timer(count_to):
+    #Initiatlises the start time
+    start_time = int(time.time())
+
+    while True:
+        #Current time variable is refreshed with each loop
+        current_time = int(time.time())
+        seconds = current_time - start_time
+        if seconds <= count_to:
+            print(seconds)
+        else:
+            break
+
+#TO DO - function needs to display video; rearrange so timer is in get_prediction();
+# maybe get_prediction_from_vid could feed into get_prediction outside while loop? Or separate function to display video?
+def get_prediction_from_vid():
+
+    while True:
         
         # Capture the video frame by frame
         ret, frame = vid.read()
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
 
         #Convert the captured frame into RGB
         im = Image.fromarray(frame, 'RGB')
@@ -40,11 +73,16 @@ def get_prediction_from_vid():
         #Expand dimensions to match the 4D Tensor shape.
         img_array = np.expand_dims(img_array, axis=0)
 
+        #Wait for 3 seconds
+        timer(3)
         #Call function to get prediction from model
         prediction = get_prediction(img_array)
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
         
+        #check if prediction is anything other than 'Nothing' class
+        if prediction != 'Nothing':
+           print(prediction)
+           break
+
         # the 'q' button is set as the quitting button you may use any
         # desired button of your choice
         if cv2.waitKey(30) & 0xFF == ord('q'):
@@ -55,19 +93,4 @@ def get_prediction_from_vid():
     # Destroy all the windows
     cv2.destroyAllWindows()
 
-def timer():
-    #Initiatlises the start time
-    start_time = int(time.time())
-
-    while True:
-        #Current time variable is refreshed with each loop
-        current_time = int(time.time())
-        seconds = current_time - start_time
-        if seconds <= 3:
-            print(seconds)
-        else:
-            break
-
-timer()
-
-
+get_prediction_from_vid()
