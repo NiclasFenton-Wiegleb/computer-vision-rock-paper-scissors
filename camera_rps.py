2d4a9bfd839cefacc97fd5319d33d542f875fdb4
Niclas Fenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-03-13 20:23:21 +0000

    Resolving error calling user_wins before assignment.

--- camera_rps.py
+++ camera_rps.py
@@ -11,9 +11,7 @@ np.set_printoptions(suppress=True)
 #Initiate trained model
 model = load_model('keras_model.h5', compile=False)
 
-#Assigning variables to track computer and user wins.
-computer_wins = 0
-user_wins = 0
+
 
 # This function generates the computer's choice by selecting a random index between 0 and 2 and returning the play with the corresponding index
 def get_computer_choice():
@@ -32,7 +30,6 @@ def timer(count_to):
         current_time = int(time.time())
         seconds = current_time - start_time
         if seconds <= count_to:
-            #print(seconds)
             continue
         else:
             break
@@ -54,7 +51,7 @@ def get_prediction(video_input):
     prediction = prediction.lower()
     return prediction
 
-def get_winner(user_choice, computer_choice):
+def get_winner(user_choice, user_wins, computer_choice, computer_wins):
     if (computer_choice == 'rock' or user_choice == 'rock') and (computer_choice == 'paper' or user_choice == 'paper'):
         if user_choice == 'paper':
             user_wins += 1
@@ -70,27 +67,26 @@ def get_winner(user_choice, computer_choice):
             user_wins += 1
         else:
             computer_wins += 1
-    
 
-#TO DO - function needs to display video; rearrange so timer is in get_prediction();
-# maybe get_prediction_from_vid could feed into get_prediction outside while loop? Or separate function to display video?
-
-##FIX: Why does the code work outside of function and not within?
-
+# Play the game by accessing camera
 def play_camera_rps():
     # define a video capture object
     vid = cv2.VideoCapture(0)
 
+    #Assigning variables to track computer and user wins.
+    computer_wins = 0
+    user_wins = 0
+
     while True:
             #Wait for 3 seconds
-            print("Countdown:")
-            print(1)
+        print("Countdown:")
+        for val in range(3):
             timer(1)
-            print(2)
-            timer(1)
-            print(3)
-            timer(1)
+            print(val+1)
 
+        #get user choice
+        user_choice_list = []
+        for val in range(10):
             # Capture the video frame by frame
             ret, frame = vid.read()
 
@@ -100,7 +96,7 @@ def play_camera_rps():
             im = im.resize((224,224))
             img_array = np.array(im)
 
-            #Expand dimensions to match the 4D Tensor shape.
+            #Expand dimensions to match the 4D Tensor shape
             img_array = np.expand_dims(img_array, axis=0)
 
             # Display the resulting frame
@@ -108,33 +104,35 @@ def play_camera_rps():
 
             #Call function to get prediction from model
             user_choice = get_prediction(img_array)
-            print(user_choice)
+            user_choice_list.append(user_choice)
 
-            #Pick computer play
-            computer_choice = get_computer_choice()
-            print(computer_choice)
+        #Select the most common element in user_choice_list
+        user_choice = max(set(user_choice_list), key=user_choice_list.count)
+        print(f"User: {user_choice}")
+        #Pick computer play
+        computer_choice = get_computer_choice()
+        print(f"Computer: {computer_choice}")
             
-            #check if prediction is anything other than 'nothing' class
-            if user_choice != 'nothing':
-                get_winner(user_choice, computer_choice)
-            else:
-                continue
-            print(f"Computer wins: {computer_wins}")
-            print(f"Your wins: {user_wins}")
+        #check if prediction is anything other than 'nothing' class
+        if user_choice != 'nothing':
+            get_winner(user_choice, user_wins, computer_choice, computer_wins)
+        else:
+            continue
+        print(f"Computer wins: {computer_wins}")
+        print(f"Your wins: {user_wins}")
 
-            #break loop once a player has three victories.
-            if (computer_wins or user_wins) == 3:
-                if user_wins > computer_wins:
-                    print('You won!')
-                    break
-                else:
-                    print('You lost')
-                    break
-            # the 'q' button is set as the quitting button you may use any
-            # desired button of your choice
-            if cv2.waitKey(30) & 0xFF == ord('q'):
+        #break loop once a player has three victories.
+        if (computer_wins or user_wins) == 3:
+            if user_wins > computer_wins:
+                print('You won!')
+                break
+            else:
+                print('You lost')
                 break
 
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
     # After the loop release the cap object
     vid.release()
     # Destroy all the windows
